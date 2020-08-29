4cd80b5cdb7a00cf40929ec4308f9506985dcffa
Quentin Gruber <quentingruber@gmail.com>
2020-08-29 12:14:03 +0200

    updt for v0.5.0

--- ZoneServer.js
+++ ZoneServer.js
@@ -1,5 +1,9 @@
-const h1serv = require("h1z1-server");
-
-var Zone = new h1serv.ZoneServer(1117, "test", false);
+const H1Z1servers = require("h1z1-server");
+const { Base64 } = require("js-base64");
+var Zone = new H1Z1servers.ZoneServer(
+  1117,
+  Base64.toUint8Array("F70IaxuU8C/w7FPXY1ibXw=="),
+  false
+);
 
 Zone.start();
--- loginserver.js
+++ loginserver.js
@@ -1,10 +1,11 @@
 const H1Z1servers = require("h1z1-server");
+const { Base64 } = require("js-base64");
 var server = new H1Z1servers.LoginServer(
   295110, // <- AppID
   "dev", // <- environment
   false, // <- using MongoDB (boolean)
   1115, // <- server port
-  "dds", // <- loginkey
-  true // <- Use spam glitch
+  Base64.toUint8Array("F70IaxuU8C/w7FPXY1ibXw=="), // <- loginkey
+  true // <- SoloMode
 );
 server.start();

d0b9a92dfcc7b2f4779f8892c093fdd49139f435
Quentin Gruber <quentingruber@gmail.com>
2020-09-05 22:00:10 +0200

    remove using MongoDB arg

--- loginserver.js
+++ loginserver.js
@@ -3,7 +3,6 @@ const { Base64 } = require("js-base64");
 var server = new H1Z1servers.LoginServer(
   295110, // <- AppID
   "dev", // <- environment
-  false, // <- using MongoDB (boolean)
   1115, // <- server port
   Base64.toUint8Array("F70IaxuU8C/w7FPXY1ibXw=="), // <- loginkey
   true // <- SoloMode

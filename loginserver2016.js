9d7754653a1d3b51bd2ae858c170be1996630e99
Quentin Gruber <quentingruber@gmail.com>
2021-04-18 16:16:13 +0200

    setup stuff for 2016 client

---
+++ loginserver2016.js
@@ -0,0 +1,7 @@
+const H1Z1servers = require("h1z1-server");
+var server = new H1Z1servers.LoginServer(
+  1115, // <- server port
+  "" // <- MongoDB address ( if blank server start in solo mode )
+);
+server._protocol = new H1Z1servers.LoginProtocol("LoginUdp_11");
+server.start();

79f1b5f5b6336ee917a2ff453f8652357278a4c2
Quentin Gruber <quentingruber@gmail.com>
2021-06-12 14:03:56 +0200

    Update ZoneServer2016.js

--- ZoneServer2016.js
+++ ZoneServer2016.js
@@ -1,8 +1,7 @@
 const H1Z1servers = require("h1z1-server");
 const { Base64 } = require("js-base64");
-var Zone = new H1Z1servers.ZoneServer(
+var Zone = new H1Z1servers.ZoneServer2016(
   1117,
   Base64.toUint8Array("F70IaxuU8C/w7FPXY1ibXw==")
 );
-//server._protocol = new H1Z1servers.ZoneProtocol("ClientProtocol_948"); TODO: add 2016 ClientProtocol
 Zone.start();

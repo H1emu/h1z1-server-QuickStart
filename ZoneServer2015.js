eddfc55c4ca6c488c7e1e9b95714987bbe658803
Quentin Gruber <quentingruber@gmail.com>
2021-07-26 23:05:33 +0200

    add env vars

--- ZoneServer2015.js
+++ ZoneServer2015.js
@@ -1,8 +1,9 @@
-const H1Z1servers = require("h1z1-server");
-const { Base64 } = require("js-base64");
-var Zone = new H1Z1servers.ZoneServer(
-  1117,
-  Base64.toUint8Array("F70IaxuU8C/w7FPXY1ibXw==")
-);
+const { ZoneServer } = require("h1z1-server");
 
+const Zone = new ZoneServer(
+  1117,
+  new Buffer.from("F70IaxuU8C/w7FPXY1ibXw==", "base64"),
+  process.env.MONGO_URL,
+  Number(process.env.WORLD_ID)
+);
 Zone.start();
--- loginserver2015.js
+++ loginserver2015.js
@@ -1,6 +1,3 @@
-const H1Z1servers = require("h1z1-server");
-var server = new H1Z1servers.LoginServer(
-  1115, // <- server port
-  "" // <- MongoDB address ( if blank server start in solo mode )
-);
+const { LoginServer } = require("h1z1-server");
+const server = new LoginServer(1115, process.env.MONGO_URL);
 server.start();
